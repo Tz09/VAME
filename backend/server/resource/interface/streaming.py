b41af88c22d2b6b9b2ee7468d1a14fb9ef685968
Tz <tantiongzhi@gmail.com>
2023-07-11 21:50:17 +0800

    Exception

--- backend/server/resource/interface/streaming.py
+++ backend/server/resource/interface/streaming.py
@@ -1,22 +1,14 @@
 import cv2
-import torch
-import time
-import os
-import numpy as np
-import threading
-import torch.backends.cudnn as cudnn
-from datetime import datetime
+import traceback
 from flask_restful import Resource
 from flask import Response,request,jsonify
-from server.ai.models.experimental import attempt_load
-from server.ai.utils.general import check_img_size,non_max_suppression,scale_coords
-from server.ai.utils.datasets import letterbox,LoadStreams
-from server.ai.utils.plots import plot_one_box
-from server.ai.utils.torch_utils import select_device
-from server.service.models import db,Image,setting
+from server.ai.utils.datasets import LoadStreams
+from server.service.models import setting
 from server.resource.utils.streaming import StreamingThread
+from server.service import _state
 
 url = [('Streaming','/streaming')]
+
 dataset = LoadStreams(sources=setting["ai"]["source"], img_size=setting["ai"]["img_size"])
 streaming = StreamingThread(dataset)
 streaming.start()
@@ -25,7 +17,7 @@ def get_frame():
     while True:
         processed_frame = streaming.get_processed_frame()
         if processed_frame is None:
-            continue
+            pass
         ret, buffer = cv2.imencode('.jpg', processed_frame)
         frame_bytes = buffer.tobytes()
         yield (b'--frame\r\n'
